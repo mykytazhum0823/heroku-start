aa1ae9867cabd7bbeb176380e561320c75412f6d
mykytazhum0823 <mykytazhum@gmail.com>
2022-01-19 19:18:40 +0100

    init

---
+++ precache-manifest.980e28a8f27025b285887951b5eee295.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ec79933cfae361bee98868c0abd4c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11032bea7e3bf79b3ed2",
+    "url": "/static/css/main.e0fbd5d4.chunk.css"
+  },
+  {
+    "revision": "ea74b258f62b20fd154f",
+    "url": "/static/js/2.5734bdd7.chunk.js"
+  },
+  {
+    "revision": "e5ed1687933fbf5635de77a5402b90e3",
+    "url": "/static/js/2.5734bdd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11032bea7e3bf79b3ed2",
+    "url": "/static/js/main.ab4f25f6.chunk.js"
+  },
+  {
+    "revision": "07feb53890f831d66a36",
+    "url": "/static/js/runtime-main.c5851211.js"
+  },
+  {
+    "revision": "f43fe28a7319235f3238be0ec6607e54",
+    "url": "/static/media/Rectangle100.f43fe28a.png"
+  },
+  {
+    "revision": "56a8c7c8783496940f58d656aaa89541",
+    "url": "/static/media/Rectangle187.56a8c7c8.png"
+  },
+  {
+    "revision": "70fcb5092902bf99dbb58c6c344040f5",
+    "url": "/static/media/Rectangle201.70fcb509.png"
+  },
+  {
+    "revision": "3065ab92122efac8d0adb20275441f9b",
+    "url": "/static/media/Rectangle203.3065ab92.png"
+  },
+  {
+    "revision": "312c915bed5a0f43f8842f5760dd9c74",
+    "url": "/static/media/Rectangle205.312c915b.png"
+  },
+  {
+    "revision": "2ca1e78eeddee34691b2efe7b322efa3",
+    "url": "/static/media/Rectangle207.2ca1e78e.png"
+  },
+  {
+    "revision": "d3eb8d3bfcf7fc305e34a60b9ad12501",
+    "url": "/static/media/Rectangle209.d3eb8d3b.png"
+  }
+]);
